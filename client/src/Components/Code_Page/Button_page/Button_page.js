292ba7c7abb9c7c4701904280f4edd5ab68998f1
Haile-08 <haile4cmd@gmail.com>
2023-01-13 13:29:44 +0300

    Interface finished

---
+++ client/src/Components/Code_Page/Button_page/Button_page.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../../../App.css";
+
+function Button_page() {
+  return (
+    <div className="btn_page">
+      <button>x</button>
+      <button>Var</button>
+      <button>fillColor()</button>
+    </div>
+  );
+}
+
+export default Button_page;
--- client/src/Components/Code_Page/Code_page.js
+++ client/src/Components/Code_Page/Code_page.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "../../App.css";
+import Button_page from "./Button_page/Button_page";
 import Input_page from "./Code_input_page/Input_page";
 
 function Code_page() {
   return (
     <div className="Code">
       <Input_page />
+      <Button_page />
     </div>
   );
 }
